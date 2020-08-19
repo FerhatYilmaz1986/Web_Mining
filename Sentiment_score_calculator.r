a9ed57cf65d6992711c024b6a5d0b032c9a44067
Fer <yilmazferhat1986@gmail.com>
2020-08-19 16:20:53 -0400

    Feature : Create sentiment calculator function

--- Sentiment_score_calculator.r
+++ Sentiment_score_calculator.r
@@ -14,3 +14,6 @@ sentiment<-function(doc){
   a2 <- annotate(doc, word_token_annotator, a1)
   a3 <- subset(a2, type == "word")
   doc[a3]
+  # Set the folder that has files as a working directory, so did not use path
+  poslist = readLines('positive-words.txt')
+  neglist = readLines('negative-words.txt')

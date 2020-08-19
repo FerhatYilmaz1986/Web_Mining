edebc89864ef79deaa2502d5cc8af5f67a3a147a
Fer <yilmazferhat1986@gmail.com>
2020-08-19 16:20:09 -0400

    Feature : Create sentiment calculator function

--- Sentiment_score_calculator.r
+++ Sentiment_score_calculator.r
@@ -5,3 +5,7 @@ library(NLP)
 sentiment<-function(doc){
   doc = as.String(doc)
   doc = tolower(doc)
+  #Break into sentences
+  sent_token_annotator <- Maxent_Sent_Token_Annotator(probs=TRUE)
+  a1<-annotate(doc,sent_token_annotator)
+  doc[a1]

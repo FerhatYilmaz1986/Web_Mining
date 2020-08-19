33dd917aad1e48f3fda26ebd71f0af3819dacbff
Fer <yilmazferhat1986@gmail.com>
2020-08-19 16:20:31 -0400

    Feature : Create sentiment calculator function

--- Sentiment_score_calculator.r
+++ Sentiment_score_calculator.r
@@ -9,3 +9,8 @@ sentiment<-function(doc){
   sent_token_annotator <- Maxent_Sent_Token_Annotator(probs=TRUE)
   a1<-annotate(doc,sent_token_annotator)
   doc[a1]
+  #Break into tokens
+  word_token_annotator<-Maxent_Word_Token_Annotator(probs=TRUE)
+  a2 <- annotate(doc, word_token_annotator, a1)
+  a3 <- subset(a2, type == "word")
+  doc[a3]

2b6c83572e0cd2fbc23dcb26b4f97f69cca19dc1
Fer <yilmazferhat1986@gmail.com>
2020-08-23 10:37:54 -0400

    Feature : Update cosine similarity function

--- Cosine_similarity.r
+++ Cosine_similarity.r
@@ -3,3 +3,5 @@ library(tm)
 
 cosinesim<-function(query,document){
   corpus<-create_matrix(documents,removeStopwords = T,stemWords = T)
+  corpus<- as.matrix(corpus)
+  q1matrix<-create_matrix(q1,originalMatrix = corpus,removeStopwords = T,stemWords = T)
